75755ebec4148f93b97730aa1a8ecab4ba715e48
Emmanuel Acosta <emmanuel.acosta@correo.ucu.edu.uy>
2021-05-15 19:33:08 -0300

    Van mas servicios

---
+++ src/main/java/dto/UserObject.java
@@ -0,0 +1,44 @@
+package dto;
+
+import java.sql.Date;
+
+public class UserObject {
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	private int estado;
+	private Date fecha_nacimiento;
+	
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getEstado() {
+		return estado;
+	}
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	public Date getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+	public void setFecha_nacimiento(Date fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+	
+	
+}
--- src/main/webapp/js/funciones.js
+++ src/main/webapp/js/funciones.js
@@ -1,8 +1,13 @@
-/**
- * 
- */
 
 function buscarHora(){
+	var usuario = getEstado();
+	if(usuario["estado"]==0){
+		buscarHoraCupo();
+	}else{
+		alert("Ya tiene una agenda en curso");
+	}
+}
+function buscarHoraCupo(){
 	//Deberia agregarse archivo de configuración con el enpoint
 	var cadena = "http://localhost:8089/HelloREST/rest/WebService/GetCupos";
 	//var cedula = $("#Cedula").val();
@@ -17,8 +22,33 @@ function buscarHora(){
 			alert("No se pudo procesar la solicitud")
 		},
 		success: function(respuesta){
-			respuesta = eval(respuesta)
-			alert(respuesta)
+			respuesta = eval(respuesta);
+			var obj = JSON.stringify(respuesta);
+			alert(obj);
 		}
 	})
 }
+
+function getEstado(){
+	var usuario = new Object();
+	var cedula = $("#Cedula").val();
+	var endpoint = "http://localhost:8089/HelloREST/rest/WebService/GetEstado?cedula=" + cedula;
+	$.ajax({
+		url: endpoint,
+		type: 'GET',
+		dataType: 'json',
+		data:{},
+		timeout: 10000,
+		async: false,
+		error: function(){
+			alert("No se pudo procesar la solicitud")
+		},
+		success: function(respuesta){
+			respuesta = eval(respuesta);
+			usuario = respuesta;
+		}
+	})
+	return usuario;
+	 
+}
+
