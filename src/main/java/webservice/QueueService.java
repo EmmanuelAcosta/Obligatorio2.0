4b3dc986a36d63c67cb436834d6acab544789109
Emmanuel Acosta <emmanuel.acosta@correo.ucu.edu.uy>
2021-05-25 20:58:17 -0300

    Se sube servicio consumiendo de la cola, falta hacer uno que envie a la cola y otro que elimine luego de recibir

--- src/main/java/webservice/QueueService.java
+++ src/main/java/webservice/QueueService.java
@@ -9,22 +9,44 @@ import javax.ws.rs.QueryParam;
 
 import com.google.gson.Gson;
 
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import com.amazonaws.services.sqs.model.SendMessageRequest;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.SqsClientBuilder;
+import software.amazon.awssdk.services.sqs.model.*;
+import java.util.List;
 
 @Path("/WebService")
 public class QueueService {
-	@GET
+	/*@GET
 	@Path("/GetQueue")
 	@Produces("application/json")
 	public String cupo(@QueryParam("cedula") String cedula) {
-		
+
 		SendMessageRequest send_msg_request = new SendMessageRequest()
-		        .withQueueUrl("https://sqs.us-west-1.amazonaws.com/457393350873/queue-qa.fifo")
-		        .withMessageBody("hello world")
-		        .withDelaySeconds(5);
+				.withQueueUrl("https://sqs.us-west-1.amazonaws.com/457393350873/queue-qa.fifo")
+				.withMessageBody("hello world").withDelaySeconds(5);
 		sqs.sendMessage(send_msg_request);
-		return usuario;
+		return null;
+	}*/
+
+	@GET
+	@Path("/GetQueue")
+	@Produces("application/json")
+	public String getQueue() {
+		SqsClient sqsClient = SqsClient.builder().region(Region.US_WEST_1).build();
+		try {
+			String queueUrl = "https://sqs.us-west-1.amazonaws.com/457393350873/queue-qa.fifo";
+			ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder().queueUrl(queueUrl)
+					.maxNumberOfMessages(5).build();
+			List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+			String json = new Gson().toJson(messages);
+			return json;
+		} catch (SqsException e) {
+			System.err.println(e.awsErrorDetails().errorMessage());
+			System.exit(1);
+		}
+		return null;
+
 	}
+
 }
